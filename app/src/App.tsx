06c228bc46e26ae79c0c4f20035aca7d16e3ae6e
ohmyki-mm <omae.yuki.27i@st.kyoto-u.ac.jp>
2023-08-28 10:02:03 +0900

    WIP

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -11,68 +11,48 @@ import "./App.css"
 
 import RouteLayer from "./layers/RouteLayer"
 
+import data from "./data/Railroad.json"
+
 mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN;
 
 const overlay = new MapboxOverlay({
   interleaved: true,
   layers: [RouteLayer]
 });
-const coords = [
-  [135.52163, 34.7052],
-  [135.52191, 34.70523],
-  [135.52195, 34.70523],
-  [135.52242, 34.70523],
-  [135.52293, 34.70518],
-  [135.52312, 34.70515],
-  [135.52326, 34.70513],
-  [135.52344, 34.7051],
-  [135.52361, 34.70505],
-  [135.52381, 34.70499],
-  [135.52422, 34.70483],
-  [135.52465, 34.70462],
-  [135.52476, 34.70456],
-  [135.52489, 34.70449],
-  [135.52498, 34.70443],
-  [135.52527, 34.70426],
-  [135.52549, 34.70412],
-  [135.52592, 34.70385],
-  [135.52631, 34.7036],
-  [135.52634, 34.70358],
-  [135.52647, 34.7035],
-  [135.52657, 34.70344],
-  [135.52689, 34.70325],
-  [135.52714, 34.70308],
-  [135.52745, 34.7029],
-  [135.52759, 34.70282],
-  [135.52764, 34.70279],
-  [135.52767, 34.70276],
-  [135.52809, 34.70251],
-  [135.52859, 34.70221],
-  [135.52911, 34.70188],
-  [135.5293, 34.70176],
-  [135.52932, 34.70175],
-  [135.52968, 34.70152],
-  [135.53049, 34.70102],
-  [135.53067, 34.70091],
-  [135.5316, 34.70033],
-  [135.5318, 34.7002],
-  [135.53185, 34.70016],
-  [135.53202, 34.70005],
-  [135.53224, 34.69988],
-  [135.53253, 34.6996],
-  [135.53276, 34.69932],
-  [135.53291, 34.69907],
-  [135.53298, 34.69892],
-  [135.53301, 34.69884],
-  [135.53309, 34.69867],
-  [135.53317, 34.69851],
-  [135.53334, 34.69812],
-  [135.53347, 34.69785],
-  [135.53358, 34.69761]
-]
+// const coords = [
+//   [
+//     135.52163,
+//     34.7052
+//   ],
+//   [
+//     135.52143,
+//     34.70517
+//   ],
+//   [
+//     135.52109,
+//     34.70509
+//   ],
+//   [
+//     135.52069,
+//     34.70496
+//   ],
+//   [
+//     135.52029,
+//     34.70483
+//   ]
+// ]
 // const chunk = turf.lineChunk(coords, 500, { units: 'meters' }) as FeatureCollection;
 // console.log(chunk)
 // const length = turf.length(coords, {units: 'meters'});
+
+const multiLineString = turf.multiLineString(data.features[38].geometry.coordinates)
+const flattenedCoordinates: number[][] = [];
+
+turf.coordEach(multiLineString, (coord, coordIndex, featureIndex, multiFeatureIndex, geometryIndex) => {
+    flattenedCoordinates.push(coord);
+});
+
+const coords = flattenedCoordinates
 
 const App: React.FC = () => {
   const mapContainer = useRef<HTMLDivElement | null>(null);
@@ -185,7 +165,7 @@ const App: React.FC = () => {
       refModelAngle.current = turf.bearing([refPosPastLng.current, refPosPastLat.current],[refPosLng.current, refPosLat.current]) * Math.PI/180;
       cubeRef.current.rotation.z = -refModelAngle.current
       cubeRef.current.setCoords([refPosLng.current, refPosLat.current]);
-    }, 1000);
+    }, 100);
   }, []);
 
   return (
